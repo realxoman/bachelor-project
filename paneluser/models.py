003b02b5bf360bfbff53ab0676c910f21aad9b99
alixo <fromiran98@gmail.com>
2021-10-25 14:21:05 +0330

    v.0.0.2

--- paneluser/models.py
+++ paneluser/models.py
@@ -1,6 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class User(AbstractUser):
+    phonenumber= models.CharField(max_length=50,null=True,blank=True)
+
 class packs(models.Model):
     name = models.CharField(max_length=50,null=True,blank=True)
     enable_check = models.BooleanField(default=False)
@@ -11,15 +15,33 @@ class packs(models.Model):
         return name
 
 class payment(models.Model):
-
-
+    userid = models.ForeignKey("User", on_delete=models.DO_NOTHING)
+    order_id = models.CharField(max_length=256,blank=True,null=True,unique=True)
+    amount = models.CharField(max_length=256,blank=True,null=True)
+    refnum = models.CharField(max_length=256,blank=True,null=True)
+    irtoken = models.CharField(max_length=256,blank=True,null=True)
+    transaction_id = models.CharField(max_length=256,blank=True,null=True)
+    cardnum = models.CharField(max_length=256,blank=True,null=True)
+    tracking_code = models.CharField(max_length=256,blank=True,null=True)
+    verify_check = models.BooleanField(default=False,verbose_name="Purchase okay?")
+    final_code = models.CharField(max_length=256,blank=True,null=True)
     packs = models.ForeignKey("packs")
+
     def __str__(self):
-        return transactionid
+        return tracking_code
 
 class orders(models.Model):
-
-
+    user = models.ForeignKey("User", on_delete=models.DO_NOTHING)
     packs = models.ForeignKey("packs")
     def __str__(self):
         return user.first_name + " " + user.last_name + " " + packs.name + " " + id
+
+class ticket(models.Model):
+
+    def __str__(self):
+        return subject
+    
+class ticketpm(models.Model):
+
+    def __str__(self):
+        return ticket.subject + " " + id
