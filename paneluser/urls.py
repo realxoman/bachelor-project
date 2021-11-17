d3fe027f5edd6f005a0d547d0d6edaca2dafd8cf
alixo <fromiran98@gmail.com>
2021-11-17 16:04:20 +0330

    v11/17/2021-panelcreated

--- paneluser/urls.py
+++ paneluser/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import login_,logout_,signup
+from .views import login_,logout_,signup,AdminPanel,AdminOrders,AdminProducts,AdminTickets,AdminUsers
+from django.views.generic import TemplateView
 
 app_name = "paneluser"
 urlpatterns = [
     path('', login_),
     path('logout/', logout_, name="logout_"),
     path('signup/', signup, name="signup"),
+    path('administrator/', AdminPanel.as_view() ,name="AdminPanel"),
+    path('administrator/products/', AdminProducts.as_view() ,name="AdminProducts"),
 ]
--- paneluser/views.py
+++ paneluser/views.py
@@ -3,6 +3,7 @@ from .models import User,ticket,orders,payment,packs,ticketpm
 from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponseRedirect
 from .forms import SignUpForm
+from django.views.generic import View
 # Create your views here.
 
 def signup(request):
@@ -33,14 +34,14 @@ def login_(request):
         if user is not None:
             login(request,user)
             if request.user.is_staff:
-                return HttpResponseRedirect("/mypanel/")
+                return HttpResponseRedirect("/administrator/")
             else:
                 return HttpResponseRedirect("/usercp/")
         elif user is None:
             return render(request, "login.html")
     if request.user.is_authenticated:
         if request.user.is_staff:
-            return HttpResponseRedirect("/mypanel/")
+            return HttpResponseRedirect("/administrator/")
         else:
             return HttpResponseRedirect("/usercp/")
     else:
@@ -49,3 +50,34 @@ def login_(request):
 def logout_(request):
     logout(request)
     return HttpResponseRedirect("/")
+
+
+class AdminPanel(View):
+    template_name = "dashboard.html"
+    
+    def get(self,request):
+        return render(request,self.template_name)
+    
+class AdminProducts(View):
+    template_name = "products.html"
+    
+    def get(self,request):
+        return render(request,self.template_name)
+    
+class AdminOrders(View):
+    template_name = "dashboard.html"
+    
+    def get(self,request):
+        return render(request,self.template_name)
+
+class AdminUsers(View):
+    template_name = "dashboard.html"
+    
+    def get(self,request):
+        return render(request,self.template_name)
+    
+class AdminTickets(View):
+    template_name = "dashboard.html"
+    
+    def get(self,request):
+        return render(request,self.template_name)
